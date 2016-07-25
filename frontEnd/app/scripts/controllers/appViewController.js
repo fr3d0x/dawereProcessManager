62351dd8269325919e3ed181d33c3c44ddd031bf
Alfredo Escalante <alfredoescalante89@gmail.com>
2016-07-24 20:18:30 -0430

    appViewController bug fix

--- frontEnd/app/scripts/controllers/appViewController.js
+++ frontEnd/app/scripts/controllers/appViewController.js
@@ -1,8 +1,8 @@
 /**
  * Created by fr3d0 on 7/24/16.
  */
-app.controller("appViewController",['$scope', 'ENV', 'dawProcessManagerService', 'localStorageService', '$location', '$base64','$window','$state','$stateParams', 'responseHandlingService',
-    function ($scope, ENV, dawProcessManagerService, localStorageService, $location, $base64, $window,$state,$stateParams, responseHandlingService){
+app.controller("appViewController",['$scope', 'ENV', 'dawProcessManagerService', 'localStorageService', '$location', '$base64','$window','$state','$stateParams', 'responseHandlingService', '$rootScope',
+    function ($scope, ENV, dawProcessManagerService, localStorageService, $location, $base64, $window,$state,$stateParams, responseHandlingService, $rootScope){
 
 
 
