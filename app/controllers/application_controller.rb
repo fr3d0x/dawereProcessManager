433362b4b495d1342ffd63ae425a658ad1a29852
Maria Emilia <maria.risi91@gmail.com>
2017-05-07 11:26:16 -0430

    tittle libreto added fixed upload route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::API
   require 'jwt'
   require 'json'
   $secretKey = "d@w3r3's_$3cr3t_k3y"
-  $drive_copy_route = '/mnt/hfgs/NUBE DAWERE'
+  $drive_copy_route = '/mnt/hgfs/NUBE DAWERE'
   def authenticate
     token = request.headers['AUTHORIZATION']
     $currentPetitionUser = JWT.decode(token, $secretKey, true, { :algorithm => 'HS256' })[0]
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,5 +1,5 @@
 CarrierWave.configure do |config|
   #set carrierwave new root folder
-  config.root = '/mnt/hfgs/archivos_dpm/'
-  config.cache_dir = '/mnt/hfgs/archivos_dpm/tmp/uploads'
+  config.root = '/mnt/hgfs/archivos_dpm/'
+  config.cache_dir = '/mnt/hgfs/archivos_dpm/tmp/uploads'
 end
