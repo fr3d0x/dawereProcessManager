ab8ea42ea49f715206044bc84eea1a5b5a0a497b
fr3d0x <alfredoescalante89@gmail.com>
2017-05-09 11:12:14 -0430

    routes for development

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,5 +1,5 @@
 CarrierWave.configure do |config|
   #set carrierwave new root folder
-  config.root = '/mnt/hgfs/archivos_dpm/'
-  config.cache_dir = '/mnt/hgfs/archivos_dpm/tmp/uploads'
+  config.root = '/home/fr3d0/Desktop/uploads/rails/dpm'
+  config.cache_dir = '/home/fr3d0/Desktop/uploads/rails/dpm/tmp/uploads'
 end
